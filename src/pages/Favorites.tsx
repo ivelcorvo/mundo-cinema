22fda27ea26e9abd953fc6e718a257475696bdd5
ivelcorvo <levialvesjr@hotmail.com>
2025-10-02 15:45:59 -0300

    feat: adiciona o conteúdo da página Favotites

--- src/pages/Favorites.tsx
+++ src/pages/Favorites.tsx
@@ -1,7 +1,44 @@
+import { useMovies, TMDBMovie } from "../hooks/useMovies"
+import { useFavorites } from "../hooks/useFavorites"
+import { useEffect, useState } from "react";
+import Loading from "../components/Loading";
+import Movies from "../components/Movies";
 
 const Favorites = () => {
+
+  const {favorites,addIdFavorites,removeFavorite} = useFavorites();
+  const {getMoviesByIds,error,loading} = useMovies();
+
+  const [movies,setMovies] = useState<TMDBMovie[]>([]);
+
+  useEffect(()=>{
+    const loadMovie = async()=>{
+      const data :TMDBMovie[] = await getMoviesByIds(favorites);
+      setMovies(data);
+    }
+    loadMovie();
+  },[getMoviesByIds,favorites]);
+  console.log(favorites);
+  console.log(movies);
+
   return (
-    <div>Favorites</div>
+    <>
+
+      {(loading&&!error) &&
+        <Loading></Loading>
+      }
+      {(!error&&!loading&&movies.length>0) &&
+        <Movies
+        movies={movies}
+        favorites={favorites}
+        addIdFavorites={addIdFavorites}
+        removeFavorite={removeFavorite}
+        webPage={"favorites"}
+        ></Movies>
+      }
+      {error && <p><i className="fa-solid fa-face-frown"></i> Infelizmente não foi possível trazer os filmes...</p>}
+    
+    </>
   )
 }
 
